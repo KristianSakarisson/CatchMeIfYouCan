09afa9b713f887ed605d0526fbd4a0099726e816
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-10 21:59:42 +0100

    Added player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveConstant = .2f;
+
+	void Start ()
+    {
+	
+	}
+	
+	void FixedUpdate ()
+    {
+        float verticalMove = Input.GetAxis("Vertical") * moveConstant;
+        float horizontalMove = Input.GetAxis("Horizontal") * moveConstant;
+
+        transform.Translate(horizontalMove, verticalMove, 0);
+	}
+}
