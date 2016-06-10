478a86b34bbc0ee4ed1926d1ce52a03bf10d8577
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-11 00:59:26 +0100

    Fixed CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    GameObject player;
 
     Vector3 differenceZaxis;
 
     void Start()
     {
+        player = GameObject.Find("player");
         differenceZaxis = new Vector3(0, 0, Camera.main.transform.position.z - player.transform.position.z);
     }
 	
 	void Update ()
-    {
+    { 
         Camera.main.transform.position = player.transform.position + differenceZaxis;
 	}
 
--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -3,19 +3,32 @@ using System.Collections;
 
 public class Initialize : MonoBehaviour
 {
+    public int mapSize = 8;
     public GameObject[] roomOutlines;
+    public GameObject player;
     private int[] rotations = new int[] { 0, 90, 180, 270 };
 
-	void Start ()
+	void Awake ()
     {
-        for (int i = 0; i < 8; i++)
+        int[] spawnPosition = new int[] {Random.Range(0, mapSize), Random.Range(0, mapSize) };
+        GameObject parent = new GameObject("rooms");
+        for (int i = 0; i < mapSize; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < mapSize; j++)
             {
                 Vector3 spawnVector = new Vector3(transform.position.x + i * 1.48f, transform.position.y + j * 1.48f, transform.position.z);
                 GameObject newRoom = Instantiate(roomOutlines[Random.Range(0, roomOutlines.Length)], spawnVector, Quaternion.identity) as GameObject;
 
                 newRoom.transform.Rotate(new Vector3(0, 0, rotations[Random.Range(0, rotations.Length)]));
+
+                newRoom.transform.parent = parent.transform;
+
+                if(spawnPosition[0] == i && spawnPosition[1] == j)
+                {
+                    GameObject playerSpawned = Instantiate(player, spawnVector, Quaternion.identity) as GameObject;
+
+                    playerSpawned.name = "player";
+                }
             }
         }
 	}
