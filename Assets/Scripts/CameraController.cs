288e136744a17e284e3c23f46c2ead60b45a4a27
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-11 00:10:54 +0100

    Added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    Vector3 differenceZaxis;
+
+    void Start()
+    {
+        differenceZaxis = new Vector3(0, 0, Camera.main.transform.position.z - player.transform.position.z);
+    }
+	
+	void Update ()
+    {
+        Camera.main.transform.position = player.transform.position + differenceZaxis;
+	}
+
+    public void SetPlayer(GameObject input)
+    {
+        player = input;
+    }
+}
