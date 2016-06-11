43b659091b9db96291262f5853a0d16402f6d60d
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-11 15:24:29 +0100

    Changed playercontroller to use velocity instead of addForce and added collides to door and wall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveConstant = 70f;
+    public float moveConstant;
 
 	void FixedUpdate() 
 	{
         float verticalMove = Input.GetAxis("Vertical") * moveConstant;
         float horizontalMove = Input.GetAxis("Horizontal") * moveConstant;
 
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalMove, verticalMove));
-	}
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove, verticalMove);
+    }
 }
