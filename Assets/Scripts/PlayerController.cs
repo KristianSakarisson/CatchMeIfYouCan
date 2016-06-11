7093985628fbce0f5cd325f9d13677e9246ad1ec
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-11 22:31:12 +0100

    Added player sprite and made it rotate!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,5 +11,8 @@ public class PlayerController : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal") * moveConstant;
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove, verticalMove);
+
+        if(verticalMove + horizontalMove > 0f || verticalMove + horizontalMove < 0f)
+            transform.rotation = Quaternion.FromToRotation(Vector2.right, new Vector2(horizontalMove, verticalMove));
     }
 }
