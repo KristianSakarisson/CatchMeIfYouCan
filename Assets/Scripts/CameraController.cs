3f8848c458d4fa595d6b9118ccd4eaf1fe9e5183
Jóannes á Fløtti <info@greatedesign.com>
2016-06-11 14:10:11 +0100

    changes...

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,27 +6,29 @@ public class CameraController : MonoBehaviour
     GameObject player;
     Vector3 differenceZaxis;
 
-	public float zoomSizeMin = 1;
-	public float zoomSizeMax = 4;
+	public float currentOrtho; // Value of most current Camera.main.orthographicSize
 
-	public float duration = 2.0f;
-	public float zoom = 0.8f;
+	public float zoom	 	= 1; // Zoom speed
+	public float smooth 	= 1.0f; // Smoothness of zooming
+	public float minOrtho 	= 1.0f; // Minimum of zoom
+	public float maxOrtho 	= 6.0f; // Maximum of zoom
 
     void Start()
     {
-        player = GameObject.Find("player");
+		currentOrtho = Camera.main.orthographicSize;
+		player = GameObject.Find("player");
         differenceZaxis = new Vector3(0, 0, Camera.main.transform.position.z - player.transform.position.z);
     }
 	
 	void Update ()
     { 
-		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-			zoomOut();
-		}
-		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
-			zoomIn();
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		if (scroll != 0.0f) {
+			currentOrtho -= scroll * zoom;
+			currentOrtho = Mathf.Clamp (currentOrtho, minOrtho, maxOrtho);
 		}
 
+		ZoomView ();
 		Camera.main.transform.position = player.transform.position + differenceZaxis;
 	}
 
@@ -35,16 +37,9 @@ public class CameraController : MonoBehaviour
         player = input;
     }
 
-	public void zoomOut() 
+	public void ZoomView()
 	{
-		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, Mathf.Clamp(Camera.main.orthographicSize+zoom, zoomSizeMin, zoomSizeMax), Time.deltaTime / duration);
+		Camera.main.orthographicSize = Mathf.Lerp (Camera.main.orthographicSize, currentOrtho, smooth * Time.deltaTime);
 	}
-	public void zoomIn() 
-	{
-		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, Mathf.Clamp(Camera.main.orthographicSize-zoom, zoomSizeMin, zoomSizeMax), Time.deltaTime / duration);
-	}
-	public void canZoom() 
-	{
-		Debug.Log(Camera.main.orthographic);
-	}
+		
 }
