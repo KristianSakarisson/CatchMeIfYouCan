69b17f03ef831d37ded8e569c93867d6d27ebfb3
Jóannes á Fløtti <info@greatedesign.com>
2016-06-12 13:34:10 +0100

    Sound on player added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,15 @@ public class PlayerController : NetworkBehaviour
     public float moveConstant;
 	public Animator animator;
 
+	public AudioClip walking, running;
+
+	private AudioSource source;
+
+
 	void Start ()
 	{
 		animator = gameObject.GetComponent<Animator> ();
+		source = GetComponent<AudioSource> ();
 	}
 
     void FixedUpdate() 
@@ -24,10 +30,28 @@ public class PlayerController : NetworkBehaviour
 
         if (verticalMove + horizontalMove > 0f || verticalMove + horizontalMove < 0f) {
 			transform.rotation = Quaternion.FromToRotation (Vector2.right, new Vector2 (horizontalMove, verticalMove));
+
+			if (!source.isPlaying) {
+				source.PlayOneShot (walking, 1F);
+			}
+
+			moveConstant = 2f;
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				source.Stop ();
+			}
+			if (Input.GetKey (KeyCode.Space)) {
+				moveConstant = 6f;
+				if (source.isPlaying)
+					return;
+				source.Play (running, 1F);
+			}
+
 			animator.SetBool ("isWalking", true);
 		} 
 		else 
 		{
+			source.Stop ();
 			animator.SetBool("isWalking", false);
 		}
     }
