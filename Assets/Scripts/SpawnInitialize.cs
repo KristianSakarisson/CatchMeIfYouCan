60d3816b312a0259bc1dd3d2ecd5d882a5bb9106
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-12 10:37:20 +0100

    Added player types and stuff

--- Assets/Scripts/SpawnInitialize.cs
+++ Assets/Scripts/SpawnInitialize.cs
@@ -11,19 +11,11 @@ public class SpawnInitialize : NetworkBehaviour {
     [SyncVar]
     public int seed;
 
-	void Start () 
-	{
-        if (isServer)
-            seed = Random.Range(0, 10);
-        else if (isClient)
-            Debug.Log("seed: " + seed + ", random.seed: " + Random.seed);
+    IEnumerator WaitBeforeInitialize(float time)
+    {
+        yield return new WaitForSeconds(time);
 
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-
-        Random.seed = seed;
+        Debug.Log("Hello!");
 
         statistics = GameObject.Find("Scripts").GetComponent<Statistics>();
 
@@ -31,7 +23,48 @@ public class SpawnInitialize : NetworkBehaviour {
 
         statistics.player = gameObject;
 
-        myCamera = (Transform)Instantiate (cam, transform.position + Vector3.back, transform.rotation);
-		myCamera.GetComponent<CameraController>().player = gameObject;
+        myCamera = (Transform)Instantiate(cam, transform.position + Vector3.back, transform.rotation);
+        myCamera.GetComponent<CameraController>().player = gameObject;
+    }
+
+	void Start () 
+	{
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        statistics = GameObject.Find("Scripts").GetComponent<Statistics>();
+
+        statistics.playerType = Statistics.PlayerType.seeker;
+
+        if (isServer)
+        {
+            seed = 2;
+            statistics.seed = seed;
+            statistics.playerType = Statistics.PlayerType.hider;
+        }
+
+        StartCoroutine(WaitBeforeInitialize(.05f));
 	}
+
+    float time = 0f;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (isLocalPlayer)
+        {
+            //Debug.Log(statistics.playerType);
+            //Debug.Log(Random.seed);
+        }
+
+        if(isServer)
+        {
+            //seed++;
+            //Debug.Log("Server");
+            //time = 0f;
+        }
+    }
 }
