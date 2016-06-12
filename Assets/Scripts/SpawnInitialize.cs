de006102c54a972fdeafaef8e6713c95627f5657
Kristian Sakarisson <kristian@sakarisson.com>
2016-06-12 03:00:15 +0100

    Fixed SpawnInitialize

--- Assets/Scripts/SpawnInitialize.cs
+++ Assets/Scripts/SpawnInitialize.cs
@@ -7,18 +7,19 @@ public class SpawnInitialize : NetworkBehaviour {
 	public Transform cam;
 	public Transform myCamera;
 
-	// Use this for initialization
 	void Start () 
 	{
-		GameObject.Find ("Scripts").GetComponent<Statistics> ().player = gameObject;
-		if (!isLocalPlayer) {
+		if (!isLocalPlayer)
+        {
 			return;
 		}
-		myCamera = (Transform)Instantiate (cam, transform.position + Vector3.back, transform.rotation);
+
+        GameObject.Find("Scripts").GetComponent<Statistics>().player = gameObject;
+
+        myCamera = (Transform)Instantiate (cam, transform.position + Vector3.back, transform.rotation);
 		myCamera.GetComponent<CameraController> ().player = gameObject;
 	}
 
-	// Update is called once per frame
 	void Update () {
 	}
 }
